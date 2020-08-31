6be3787d265cdb0d0d764f1c52bda00d75394311
mykim2020 <mykim8710@gmail.com>
2020-08-31 21:45:24 +0900

    First Commit

---
+++ src/main/java/io/mykim/sampleBoot/SpringBootProjectBcApplication.java
@@ -0,0 +1,13 @@
+package io.mykim.sampleBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectBcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectBcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mykim/sampleBoot/SpringBootProjectBcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mykim.sampleBoot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectBcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
